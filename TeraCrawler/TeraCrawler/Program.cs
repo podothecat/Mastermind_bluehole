cb130887da5bcdf5819b8fb04c0e654102ffe01f
hyunjong.lee <hyunjong.lee.s@gmail.com>
2014-07-15 14:19:31 +0900

    Initialize Project
    Attached library
    - LinqConnect Express

---
+++ TeraCrawler/TeraCrawler/Program.cs
@@ -0,0 +1,32 @@
+﻿using DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeraCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new TeraDataContext())
+            {
+                var articleList = context.Articles.ToList();
+                context.Articles.InsertOnSubmit(
+                    new Article
+                    {
+                        CrawledTime = DateTime.Now,
+                        Author = "",
+                        Title = "",
+                        RawHtml = "",
+                        ContentHtml = "",
+                        ArticleWrittenTime = DateTime.Now.ToString(),
+                    });
+
+                context.SubmitChanges();
+            }
+        }
+    }
+}
