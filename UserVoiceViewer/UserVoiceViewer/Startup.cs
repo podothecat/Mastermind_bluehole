ded90de027bc381b06dfad8155bdd5b09d51eb91
hyunjong.lee <hyunjong.lee.s@gmail.com>
2014-10-02 18:51:23 +0900

    웹 뷰어 프로젝트 작성
    웹 뷰어 프로젝트 개발 시작!

---
+++ UserVoiceViewer/UserVoiceViewer/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Owin;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(UserVoiceViewer.Startup))]
+namespace UserVoiceViewer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
