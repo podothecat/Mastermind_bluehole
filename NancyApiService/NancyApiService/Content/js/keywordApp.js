2d930586ff4a4e83a961f1f11fc6b74abed4f07c
hyunjong.lee <hyunjong.lee.s@gmail.com>
2014-10-16 20:32:28 +0900

    Keyword analysis API Service
    Added api service
    - technique: nancy!
    - content: keyword analysis
    - comment: go!

---
+++ NancyApiService/NancyApiService/Content/js/keywordApp.js
@@ -0,0 +1,70 @@
+﻿var keywordApp = angular.module('keywordApp', []);
+
+keywordApp.controller('KeywordController', function ($scope, KeywordService) {
+    $scope.Keywords = [
+        { "Key": "나오/VV", "Value": 77 },
+        { "Key": "엘카/NN", "Value": 71 },
+        { "Key": "생각/NN", "Value": 56 },
+        { "Key": "정도/NN", "Value": 55 },
+        { "Key": "오르/NN", "Value": 55 },
+        { "Key": "사람/NN", "Value": 54 },
+        { "Key": "모르/VV", "Value": 51 },
+        { "Key": "무기/NN", "Value": 50 }
+    ];
+
+    $scope.beginDate = "";
+    $scope.endDate ="";
+
+    $scope.getKeywords = function() {
+        console.log("called");
+
+        var beginDate = $('#beginDate').data('DateTimePicker').getDate().format("YYYY-MM-DD");
+        var endDate = $('#endDate').data('DateTimePicker').getDate().format("YYYY-MM-DD");
+        console.log(beginDate);
+        console.log(endDate);
+
+        KeywordService
+            .requestKeywords(beginDate, endDate)
+            .then(function (keywords) {
+                $scope.Keywords = keywords;
+            });
+    };
+
+    function getKeywords(beginDate, endDate) {
+        console.log("called");
+        KeywordService.requestKeywords(beginDate, endDate)
+            .then(function (keywords) {
+                $scope.Keywords = keywords;
+                $scope.$apply();
+            });
+    };
+});
+
+keywordApp.service('KeywordService', function($http, $q) {
+
+    return ({
+        requestKeywords: requestKeywords,
+    });
+
+    function requestKeywords(beginDate, endDate) {
+        var request = $http({
+            method: "get",
+            url: ("/tera/keywords/" + beginDate + "/" + endDate),
+        });
+
+        return (request.then(handleSuccess, handleError));
+    }
+
+    function handleSuccess(response) {
+        return (response.data);
+    }
+
+    function handleError(response) {
+        if (!angular.isObject(response.data) || !response.data.message) {
+            return ($q.reject("An unknown error occurred."));
+        }
+
+        // Otherwise, use expected error message.
+        return ($q.reject(response.data.message));
+    }
+});
---
+++ NancyApiService/NancyApiService/Modules/KeywordModule.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.XPath;
+using Nancy;
+using Nancy.Json;
+using NancyApiService.DataModel;
+using NancyApiService.Helper;
+using System.Xml.Linq;
+
+namespace NancyApiService
+{
+    public class KeywordModule : NancyModule
+    {
+        private TeraArticlesDataContext _dataContext = new TeraArticlesDataContext();
+
+        public KeywordModule() : base("/tera")
+        {
+            JsonSettings.MaxJsonLength = Int32.MaxValue;
+
+            Get["/article"] = _ =>
+            {
+                return Response.AsJson(
+                    _dataContext.Articles
+                        .Select(e => new
+                        {
+                            e.ArticleAutoId,
+                            e.ArticleId,
+                            e.Author,
+                            e.ContentHtml,
+                            e.Keywords,
+                        })
+                        .First());
+            };
+
+            Get["/test"] = _ =>
+            {
+                return View["Keywords.html"];
+            };
+
+            Get["/keywords/{beginDate}/{endDate}"] = _ =>
+            {
+                var beginDate = DateTime.Parse((string)_.beginDate);
+                var endDate = DateTime.Parse((string)_.endDate);
+                var keywordsList = _dataContext.Articles
+                    .Where(e => e.ArticleWrittenTime >= beginDate)
+                    .Where(e => e.ArticleWrittenTime <= endDate)
+                    .Select(e => e.Keywords)
+                    .ToList();
+
+                var wordCount = new Dictionary<string, int>();
+                foreach (var keywords in keywordsList.Where(e => !string.IsNullOrEmpty(e)))
+                {
+                    var xDoc = XDocument.Parse(keywords);
+                    var keywordList = xDoc
+                        .XPathSelectElements("//Document/Sentence")
+                        .SelectMany(e => e.Value.Split(','))
+                        .Where(e => e.Contains("/"))
+                        .Select(e => new { Word = e.Split('/')[0], Tag = e.Split('/')[1], })
+                        .Where(e => e.Word.Length > 1)
+                        .Select(e => e.Word + "/" + e.Tag)
+                        .Distinct();
+
+                    foreach (var keyword in keywordList)
+                    {
+                        if (!wordCount.ContainsKey(keyword)) wordCount.Add(keyword, 0);
+                        wordCount[keyword] = wordCount[keyword] + 1;
+                    }
+                }
+
+                return Response.AsJson(wordCount.OrderByDescending(e => e.Value).Take(50));
+            };
+        }
+    }
+}
